5303b50d7c990fbdc57fdbebfefc049c38951d35
oussamanajib <39277805+oussamanajib@users.noreply.github.com>
2022-06-09 10:58:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ io.on('connection', (socket) => {
 // server.listen(3000, () => {
 //   console.log('listening on *:3000');
 // });
-app.listen(process.env.PORT || 3000, 
+server.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
